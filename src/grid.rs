019f5b33b97f2d792a51f4ea4cd10b2bc632bd5b
aalvarado <adan.alvarado7@gmail.com>
2017-11-27 19:54:38 -0800

    Coordinates function

--- src/grid.rs
+++ src/grid.rs
@@ -8,4 +8,16 @@ impl Grid {
     pub fn new(height: usize, width: usize) -> Grid {
         Grid { cells: vec![vec![Cell::new(); width]; height] }
     }
+
+    pub fn coords(x: usize, y: usize) -> Vec<(usize, usize)> {
+        vec![
+        (x - 1, y - 1),
+        ( x - 1, y),
+        (x - 1, y + 1),
+        (x, y - 1),
+        (x, y + 1),
+        (x + 1, y - 1),
+        (x + 1, y),
+        (x + 1, y + 1)]
+    }
 }
