32fa336b4e65797ec6f846bb8c89d010e31635af
aalvarado <adan.alvarado7@gmail.com>
2017-12-12 20:18:03 -0800

    initial methods for coords

--- src/grid.rs
+++ src/grid.rs
@@ -1,6 +1,6 @@
 use cell::Cell;
 
-const COORDINATES: [(isize, isize)] = [
+const COORDINATES: [(isize, isize); 8] = [
     (-1, -1),
     (-1, 0),
     (-1, 1),
@@ -9,17 +9,26 @@ const COORDINATES: [(isize, isize)] = [
     (1, -1),
     (1, 0),
     (1, 1)
-]
+];
 
 pub struct Grid {
-    cells: Vec<Vec<Cell>>
+    cells: Vec<Vec<Cell>>,
+    height: 0,
+    width: 0
 }
 
 impl Grid {
     pub fn new(height: usize, width: usize) -> Grid {
-        Grid { cells: vec![vec![Cell::new(); width]; height] }
+        Grid {
+            cells: vec![vec![Cell::new(); width]; height],
+            height: height,
+            width: width
+        }
     }
 
-    pub fn coords(x: usize, y: usize) -> Vec<(usize, usize)> {
+    pub fn coords(&self, x: usize, y: usize) -> () {
+    }
 
+    pub fn neighborg_count(&self) -> () {
+    }
 }
